5feeff87fc95a2771720fc6059d75e5e252a497b
Caio Gomes <ocaioluis@gmail.com>
2023-03-28 01:05:45 +0100

    fix(ui): prevent displacement when content overflows

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
                 universCondensed.variable
             )}
         >
-            <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto">
+            <body className="antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto overflow-y-scroll">
                 <Sidebar />
                 <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0">
                     {children}
